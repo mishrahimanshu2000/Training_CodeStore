c219671a5ccf793611bfb0657de6f37c963fb7a6
Himanshu Mishra <mishrahimanshu2000@gmail.com>
2023-02-24 16:37:47 +0530

    Changed

--- 24-02-2023/Task3.js
+++ 24-02-2023/Task3.js
@@ -1,28 +1,41 @@
 function fetch() {
+    validate();
     var name = document.getElementById('name').value;
     var age = document.getElementById('age').value;
     var address = document.getElementById('address').value;
     var contact = document.getElementById('contact').value;
     var gen = getGender();
-    document.getElementById('displayMessage').innerHTML = "Name : "+ name +"<br> Age : "+ age + "<br> Address : " + address + "<br> Contact : " + contact + "<br> Gender : " + gen ;
+    document.getElementById('displayMessage').innerHTML = "Name : " + name + "<br> Age : " + age + "<br> Address : " + address + "<br> Contact : " + contact + "<br> Gender : " + gen;
     checkbox();
 }
-function getGender()  {
+function getGender() {
     var gender = document.getElementsByName('gender');
-    for(i=0; i<gender.length; i++){
-        if(gender[i].checked){
+    for (i = 0; i < gender.length; i++) {
+        if (gender[i].checked) {
             return gender[i].value;
         }
     }
 }
-function checkbox(){
+function checkbox() {
     var cb = document.getElementsByName('checkbox');
     var res = "";
-    for(var i=0; i<cb.length; i++){
-        if(cb[i].checked){
+    for (var i = 0; i < cb.length; i++) {
+        if (cb[i].checked) {
             res += cb[i].value + " <br>";
         }
     }
     document.getElementById('subs').innerHTML = "Selected Subjects are : <b> <br>" + res + "</b>";
-    
+
+}
+function validate() {
+    var x = document.forms["regForm"]["name"].value;
+    var num = document.forms["regForm"]["contact"].value;
+    var rgx =  /^\[1-9][0-9]{10}$/;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    rgx.test(num);
+
+
 }
