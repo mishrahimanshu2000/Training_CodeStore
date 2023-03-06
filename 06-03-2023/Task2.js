c27f0e6a7a2caf8ccc94dd5f241e71f2cac6e936
Himanshu Mishra <mishrahimanshu2000@gmail.com>
2023-03-06 12:25:53 +0530

    Task 2

---
+++ 06-03-2023/Task2.js
@@ -0,0 +1,70 @@
+//An access modifier is a keyword that we can apply to a member of a class to control its access from the outside.
+// The followings are the access modifiers in most of the object-oriented programs.
+// public, private and protected.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Employee = /** @class */ (function () {
+    // employeeName;
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning ".concat(this.employeeName));
+    };
+    Object.defineProperty(Employee.prototype, "salary", {
+        get: function () {
+            return this._salary;
+        },
+        set: function (s) {
+            this._salary = s;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // private function
+    Employee.prototype.isManager = function () {
+        return this._salary > 20000;
+    };
+    return Employee;
+}());
+var CodeStore_Employee = /** @class */ (function (_super) {
+    __extends(CodeStore_Employee, _super);
+    function CodeStore_Employee(name) {
+        return _super.call(this, name) || this;
+    }
+    CodeStore_Employee.prototype.setSalary = function (s) {
+        this._salary = s; // protected member is accessed by subClass;
+    };
+    return CodeStore_Employee;
+}(Employee));
+var prateek = new Employee("Prateek");
+prateek.greet();
+prateek.salary = 12000;
+console.log(prateek.salary);
+// prateek.isManager();
+// this will throw an error : Property 'isManager' is private and only accessible within class 'Employee'.
+console.log(prateek.employeeName);
+// prateek._salary;
+// this will also throw an errior: Property '_salary' is protected and only accessible within class 'Employee' and its subclasses.
+var himanshu = new CodeStore_Employee("Himanshu");
+console.log(himanshu.employeeName);
+himanshu.greet();
+// himanshu.isManager(); 
+// will throw an error;
+// himanshu._salary;
+// this will also throw an errior: Property '_salary' is protected and only accessible within class 'Employee' and its subclasses.
+himanshu.setSalary(15000);
+console.log(himanshu.salary);
---
+++ 06-03-2023/Task2.ts
@@ -0,0 +1,61 @@
+//An access modifier is a keyword that we can apply to a member of a class to control its access from the outside.
+// The followings are the access modifiers in most of the object-oriented programs.
+// public, private and protected.
+
+
+class Employee {
+    employeeName: string
+
+    // protected member
+    protected _salary : number;
+    // employeeName;
+    constructor(name: string) {
+        this.employeeName = name
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`)
+    }
+    
+    public get salary() : number {
+        return this._salary;
+    }
+    public set salary(s : number){
+        this._salary = s;
+    }
+    // private function
+    private isManager() {
+        return this._salary > 20000;
+    }
+}
+class CodeStore_Employee extends Employee{
+    constructor(name : string){
+        super(name);
+    }
+    public setSalary(s : number){
+        this._salary = s;  // protected member is accessed by subClass;
+    }
+}
+
+let prateek = new Employee("Prateek");
+
+prateek.greet();
+prateek.salary = 12000;
+console.log(prateek.salary)
+// prateek.isManager();
+    // this will throw an error : Property 'isManager' is private and only accessible within class 'Employee'.
+console.log(prateek.employeeName);
+// prateek._salary;
+    // this will also throw an errior: Property '_salary' is protected and only accessible within class 'Employee' and its subclasses.
+
+let himanshu = new CodeStore_Employee("Himanshu");
+console.log(himanshu.employeeName);
+himanshu.greet();
+// himanshu.isManager(); 
+    // will throw an error;
+
+// himanshu._salary;
+    // this will also throw an errior: Property '_salary' is protected and only accessible within class 'Employee' and its subclasses.
+
+himanshu.setSalary(15000);
+console.log(himanshu.salary);
