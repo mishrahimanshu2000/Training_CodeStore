eb20e6f53800c319d7672889999424416fe31db9
Himanshu Mishra <mishrahimanshu2000@gmail.com>
2023-03-13 13:08:21 +0530

    Task 3

---
+++ 13-03-2023/Task3/Task3.js
@@ -0,0 +1,32 @@
+var arr = ["1", "a", "2", "b", "3", "c", "4", "d", "5", "e"]; // Given Array
+var ans = []; // New Array
+console.log("Given Array");
+console.log(arr);
+arr.forEach(function (element) {
+    console.log(element + " -> " + typeof element);
+});
+arr.forEach(function (element) {
+    if (isNaN(parseInt(element)))
+        ans.push(element.toUpperCase());
+    else
+        ans.push(parseInt(element));
+});
+console.log("Modifying\n");
+for (var i = 0; i < arr.length; i++) {
+    if (isNaN(parseInt(arr[i]))) {
+        arr[i] = arr[i].toUpperCase();
+    }
+    else {
+        arr[i] = parseInt(arr[i]);
+    }
+}
+console.log("After Modification");
+console.log(arr);
+arr.forEach(function (element) {
+    console.log(element + " -> " + typeof element);
+});
+console.log("New Array");
+console.log(ans);
+ans.forEach(function (element) {
+    console.log(element + " -> " + typeof element);
+});
---
+++ 13-03-2023/Task3/Task3.ts
@@ -0,0 +1,38 @@
+let arr : any = ["1", "a", "2", "b", "3", "c", "4", "d", "5", "e"];   // Given Array
+let ans : Array<Number | string> = [];  // New Array
+
+console.log("Given Array");
+console.log(arr);
+arr.forEach((element) => {
+    console.log(element + " -> " + typeof element)
+});
+
+// arr.forEach(element => {
+//     if(isNaN(parseInt(element)))
+//         ans.push(element.toUpperCase());
+//     else
+//         ans.push(parseInt(element));
+// });
+
+console.log("Modifying\n")
+for(let i=0; i<arr.length; i++){
+    if(isNaN(parseInt(arr[i]))){
+        arr[i] = arr[i].toUpperCase();
+    }
+    else{
+        arr[i] = parseInt(arr[i]);
+    }
+}
+console.log("After Modification");
+console.log(arr);
+arr.forEach((element) => {
+    console.log(element + " -> " + typeof element)
+});
+
+
+
+// console.log("New Array");
+// console.log(ans);
+// ans.forEach(element => {
+//     console.log(element + " -> " + typeof element)
+// });
